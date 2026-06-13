aabdabbcdace12d60c63ee499b03ee35d7dd1df7
gabrielvfdelima <gabrielvfdelima@gmail.com>
2026-06-13 11:10:08 -0300

    feat: arruma as imagens no card e adiciona o 'arraste a tela'

--- assets/js/conteudo.js
+++ assets/js/conteudo.js
@@ -1,8 +1,10 @@
 const botao = document.getElementById("mostrarAplicacoes");
 const aplicacoes = document.getElementById("aplicacoes");
+const mensagemArrastar = document.getElementById("arrastarTela");
 
 botao.addEventListener("click", () => {
   aplicacoes.classList.toggle("d-none");
+  mensagemArrastar.classList.toggle("d-none");
   botao.textContent = aplicacoes.classList.contains("d-none")
     ? "Mostrar aplicações práticas"
     : "Ocultar aplicações práticas";
